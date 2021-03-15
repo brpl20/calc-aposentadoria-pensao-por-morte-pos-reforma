0c3acf249a2c3de6cff0772fc7806a61850aa102
Bruno <pelli.br@gmail.com>
2021-03-15 19:13:41 -0300

    atualizado front alguma coisa

--- frontend/main.js
+++ frontend/main.js
@@ -6,6 +6,7 @@ function calc(user_year, user_value) {
 		return "Valor Menor do Que o Mínimo, recomecar";
 	else if (min_ano == user_value)
 		return "Valor igual ao minimo - benefício fixado no mínimo";
+
 	let [result, logs] = calcFunction(min_ano, user_value);
 
 	if (result !== undefined && result !== NaN)
@@ -20,22 +21,22 @@ function calcFunction(min_ano, user_value) {
 	if (sobra <= 1) {
 		return [(min_ano + ((sobra*min_ano)*0.60)), logs];
 	} else if (sobra > 1 && sobra <= 2) {
-		logs += `<br/>Valor Do Mínimo ${min_ano}`;
-		logs += `<br/>Valor da Primeira Sobra: ${(1*min_ano)*0.6}`;
-		logs += `<br/>Valor da Segunda Sobra: ${((sobra-1)*min_ano)*0.4}`;
+		logs += `<br/>Valor Do Mínimo R$: ${min_ano}`;
+		logs += `<br/>Valor da Primeira Sobra (60%) R$: ${(1*min_ano)*0.6}`;
+		logs += `<br/>Valor da Segunda Sobra (40%) R$: ${((sobra-1)*min_ano)*0.4}`;
 		return [(((1*min_ano)*0.6) + min_ano + (((sobra-1)*min_ano)*0.4)), logs];
 	} else if (sobra > 2 && sobra <= 3) {
-		logs += `<br/>Valor Do Mínimo ${min_ano}`;
-		logs += `<br/>Valor da Primeira Sobra: ${(1*min_ano)*0.6}`;
-		logs += `<br/>Valor da Segunda Sobra: ${(1*min_ano)*0.4}`;
-		logs += `<br/>Valor da Terceira Sobra: ${((sobra-2)*min_ano)*0.2}`;
+		logs += `<br/>Valor Do Mínimo R$: ${min_ano}`;
+		logs += `<br/>Valor da Primeira Sobra (60%): ${(1*min_ano)*0.6}`;
+		logs += `<br/>Valor da Segunda Sobra (40%): ${(1*min_ano)*0.4}`;
+		logs += `<br/>Valor da Terceira Sobra (20%): ${((sobra-2)*min_ano)*0.2}`;
 		return [(((1*min_ano)*0.6) + min_ano + ((1*min_ano)*0.4) + (((sobra-2)*min_ano)*0.2)), logs];
 	}
 	else if (sobra > 3) {
 		logs += `<br/>Valor Do Mínimo ${min_ano}`;
-		logs += `<br/>Valor da Primeira Sobra: ${(1*min_ano)*0.6}`;
-		logs += `<br/>Valor da Segunda Sobra: ${(1*min_ano)*0.4}`;
-		logs += `<br/>Valor da Terceira Sobra: ${(1*min_ano)*0.2}`;
+		logs += `<br/>Valor da Primeira Sobra (60%): ${(1*min_ano)*0.6}`;
+		logs += `<br/>Valor da Segunda Sobra (40%): ${(1*min_ano)*0.4}`;
+		logs += `<br/>Valor da Terceira Sobra (20%): ${(1*min_ano)*0.2}`;
 		logs += `<br/>Resto da Sobra: ${(user_value - (4*min_ano))*0.1}`;
 		return [( ((1*min_ano)*0.6) + min_ano + ((1*min_ano)*0.4) + ((1*min_ano)*0.2) + ((user_value - (4*min_ano))*0.1) ), logs];
 	}
